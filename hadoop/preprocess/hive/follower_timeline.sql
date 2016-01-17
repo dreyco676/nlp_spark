b5857d4506d59ccd30c3b204037140b34e1236a1
dreyco676 <dreyco676@gmail.com>
2016-01-16 19:28:15 -0600

    added TF IDF transformation

--- hadoop/preprocess/hive/follower_timeline.sql
+++ hadoop/preprocess/hive/follower_timeline.sql
@@ -3,6 +3,7 @@ SELECT
 get_json_object(timeline_json, '$.created_at') AS timeCreatedUTC
 ,get_json_object(timeline_json, '$.user.utc_offset') AS timeOffset
 -- remove all non-space whitespace (or remove delimiter collisions)
+,get_json_object(timeline_json, '$.id') AS tweetID
 ,regexp_replace(regexp_replace(get_json_object(timeline_json, '$.text'),'\n',' '),'\t',' ') AS tweetText
 ,get_json_object(timeline_json, '$.favorite_count') AS favoriteCount
 ,get_json_object(timeline_json, '$.retweet_count') AS retweetCount
